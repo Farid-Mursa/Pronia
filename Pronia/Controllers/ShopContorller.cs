275d5847196cfa2ccf8f6d7193147d71355025f6
Farid-Mursa <faridfmur@code.edu.az>
2023-03-24 14:16:31 +0400

    Format etmisdim deye gec atdim

---
+++ Pronia/Controllers/ShopContorller.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pronia.DAL;
+using Pronia.Entities;
+
+namespace Pronia.Controllers
+{
+    public class ShopContorller:Controller
+    {
+        readonly ProniaDbContext _context;
+
+        public ShopContorller(ProniaDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Detail(int id)
+        {
+            Plant? plant = _context.Plants.
+                Include(b => b.PlantDeliveryInformation).
+                Include(b => b.PlantCategories).ThenInclude(b => b.Category).
+                Include(b => b.PlantTags).ThenInclude(b => b.Tag).
+                Include(b => b.PlantImages).
+                SingleOrDefault(b => b.Id == id);
+            return View(plant);
+        }
+    }
+}
